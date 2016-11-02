2237dc2e774e3f731f71061c5374df9c943f70e7
Kris Kuiper <kris.kuiper@telefoonboek.nl>
2016-11-02 20:30:25 +0100

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,295 @@
 # Nerms
-NodeJS Entity Relationship Models
+Nerms; Nodejs Entities, Relationships Models. This module can be used to maintain and easly find entities and their relations. The library uses models to store single entities for easy finding and accessing your data. 
+
+## Usage
+##### Installation
+```bash
+$ npm install nerms
+```
+##### Require
+To use Nerms you can simply require the module as follows:
+```javascript
+var Nerms = require('nerms');
+```
+
+## Model and entity relation
+Each entity has it own properties. For example, a User entity can have an id, first name and last name property:
+```javascript
+var properties = {id : 1, firstname : 'Kris', lastname : 'Kuiper'};
+```
+
+To store this entity, you first need to create the model. You may create a new Model by simply adding them to Nerms. We will create a User model to store user data:
+```javascript
+//Create model properties
+var properties = {id : null, firstname : null, lastname : null };
+
+//Create Model and add the properties
+var model = {name : 'User', properties : properties};
+
+//Add the model to Nerms
+Nerms.type.add(model);
+```
+
+Now you can add your entity to the model you just created:
+```javascript
+//Defining the properties
+var properties = {id : 1, firstname : 'Kris', lastname : 'Kuiper'};
+
+//Creating new User entity and adding it automatic to the User model
+var entity = new Nerms.entities.User(properties);
+```
+
+You can view your model by executing the following code:
+```javascript
+console.log(Nerms.models.User)
+```
+
+The model will have three properties:
+* list
+* push
+* find
+
+
+## The model
+##### Model list function
+To show all entities within a model you may use the list function:
+```javascript
+var entities = Nerms.models.User.list();
+
+console.log(entities); //Will output: [Entity]
+```
+As you can see, the output is an Array so you can loop through your entities if necessary:
+```javascript
+var entities = Nerms.models.User.list();
+
+for(entity in entities) {
+	console.log(entities[entitiy]);
+}
+```
+
+##### Model find function
+The find function contains every entity attribute, so you may find an entitie by its property. The User entity we created above contains an id, firstname and lastname property. So the find property also contains these three properties:
+```javascript
+console.log(Nerms.models.User.find);
+/*
+Output:
+
+Object {}
+- firstname: (value, strict)
+- id: (value, strict)
+- lastname: (value, strict)
+* /
+```
+If you want to find a user by its first name you can simply use:
+```javascript
+var entity = Nerms.models.User.find.firstname('Kris');
+console.log(entity);
+```
+
+##### Model push function
+You can add entities to an existing model by using the push function: 
+```javascript
+//Create entity
+var entity = new Nerms.entities.User({id : 1, firstname : 'Kris', lastname : 'Kuiper'});
+
+//Add entity to the User model
+Nerms.models.User.push(entity)
+```
+###### \**Important note*\*
+If you create an entity it will automaticly be pushed to the model. So if you use the above code, you will have two equal User entities.
+
+##### Model destroy function
+If you want to delete a Model with all its Entities, you may use the "destroy" function for Models. Here, too, applies that all Entities are destroyed within the Model you want to destroy. 
+```
+Nerms.type.destroy('User');
+```
+
+## The entity
+##### Creating entities
+If you creating an entity, it will be automatically be added to the model.
+```javascript
+//Define user
+var properties = {id : 1, firstname : 'Kris', lastname : 'Kuiper'};
+
+//Create entity
+var entity = new Nerms.entities.User(properties);
+```
+
+##### Map entities
+If you have a record set of, for example, multiple users that you want to add as new Entities, you can add them simply by calling the "map" function. The map function expects a single object or an Array of objects:
+```javascript
+//Define users
+var users = [
+	
+	{id : 1, firstname : 'Kris', lastname : 'Kuiper'},
+	{id : 2, firstname : 'John', lastname : 'Smith'},
+	{id : 3, firstname : 'Mary', lastname : 'Collins'}
+];
+
+//Create entities
+Nerms.map(users, 'User');
+```
+##### Get property value from entity
+Of cource you can get the value from an entity property:
+```javascript
+//Find entity
+var entity = Nerms.models.User.find.id(1);
+
+//Get firstname value
+var value = entity.firstname.get();
+
+console.log(value) //Will output: Kris
+```
+
+##### Set property value of an entity
+If you want to update the value of an entity:
+```javascript
+//Find entity
+var entity = Nerms.models.User.find.firstname('Kris');
+
+console.log(entity.firstname.get()) //Will output: Kris
+
+//Update firstname value
+entity.firstname.set('John');
+
+console.log(entity.firstname.get()) //Will output: John
+```
+
+
+##### Destroy entities
+The "destroy" function will make sure that entities will be deleted in Nerms, so you won't be able to find an Entity in a Model or as a relationship in another Entity.
+You may destroy an Entity by doing so:
+```javascript
+//Find the entity you want to destroy
+var entity = Nerms.models.User.find.id(1);
+
+//Destroy entity
+Nerms.destroy(entity);
+```
+
+##### Expire entities automatically
+Nerms gives you the power to destroy an entity automatically after a predefined time in seconds. If your entity needs to be removed from this planet in 60 seconds, you can do this:
+```
+//Create entity
+var entity = new Nerms.entities.User({id : 1, firstname : 'Kris', lastname : 'Kuiper'});
+
+Nerms.expire(entity, 60);
+```
+After 60 seconds the entity will be removed and a "expire" event is raised. You can read more about events in the "Event" section.
+
+
+
+## Relationships between entities
+#### Automatic relationships
+Imagine you have a user with multiple phone numbers and you want to get the user by a specific phone number. You have to create a relationship between the user and its phone number(s). Well, I have good news, because you can let Nerms do the work for you for maintaining those relationships. You don't even have to bother linking new users or phone numbers to each other when adding them.
+
+You may create automatic relationships between entities. So you can look up, for example, a phonenumber from a user and vice versa. Let’s see how our entity properties should look like:
+```javascript
+//User properties
+{id : 1, firstname : 'Kris', lastname : 'Kuiper', 'phone_id' : 1}
+
+//Phonenumber properties
+{id : 1, number : '0123456789', user_id : 1}
+```
+The "phone_id" is the "id" property in our Phone entity as the "user_id" is the "id" property our User entity.
+
+##### Creating the models
+We still have to tell Nerms how our relationship works. So lets create the two models:
+```javascript
+var userModel  		 = Nerms.type.add({name : 'User', properties : {id : null, firstname : null, lastname : null, phonenumbers : []}});
+var phonenumberModel = Nerms.type.add({name : 'Phonenumber', properties : {id : null, number : null, users : [] }});
+```
+As you can see, we define custom properties for our two models. Be aware that the "phone" and "user" property of the two models are type of Arrays. These arrays may contain entities as you want to store a Phone entity into a User entity and visa versa.
+
+##### Link the models
+Let’s link the two models together:
+```
+userModel.link({model : 'Phonenumber', on : ['user_id', 'id'], property : ['phonenumbers', 'users']});
+phonenumberModel.link({model : 'User', on : ['phonenumber_id', 'id'], property : ['users', 'phonenumbers']});
+```
+The "on" property are the properties of our new entities that will define the link. The "property" properties are equal to the Arrays in our earlier created models.
+
+##### Add new entities
+Now we are going to add a user and phonenumber:
+```
+//Creating new User entity and adding it automatic to the User model
+var user = new Nerms.entities.User({id : 1, firstname : 'Kris', lastname : 'Kuiper', 'phonenumber_id' : 1});
+
+//Creating new Phonenumber entity and adding it automatic to the Phonenumber model
+var phonenumber = new Nerms.entities.Phonenumber({id : 1, number : '0123456789', user_id : 1});
+```
+##### Result
+Trying to find a phonenumber by number will get you all the users that are linked:
+```
+var users = Nerms.models.Phonenumber.find.number('0123456789').users.list();
+console.log(users); 
+
+/* 
+Output:
+[ Entity {
+    id: { get: [Function], set: [Function] },
+    firstname: { get: [Function], set: [Function] },
+    lastname: { get: [Function], set: [Function] },
+    phonenumbers: { list: [Function], push: [Function] } 
+} ]
+*/
+```
+
+#### Manual relationships
+You may create your own custom relationships by adding entities to other entities. 
+```javascript
+var user = {}
+var user =  Nerms.models.User.find.id(1);
+
+//User properties
+{id : 1, firstname : 'Kris', lastname : 'Kuiper', 'phone_id' : 1}
+
+//Phonenumber properties
+{id : 1, number : '0123456789', user_id : 1}
+```
+
+
+## Events
+##### On event
+Nerms will trigger events based on adding, expiring and destroying entities:
+
+```javascript
+Nerms.on('add', function(entity) {
+	console.log(entitiy);
+});
+
+Nerms.on('expire', function(entity) {
+	console.log(entitiy);
+});
+
+Nerms.on('destroy', function(entity) {
+	console.log(entitiy);
+});
+```
+
+###### \**Important note*\*
+If an entity is expired it will raise the "expire" event, but also the "destroy" event because of the fact that when an Entity is expired, it will also be destroyed. 
+
+##### Off event
+When you no longer want to listen to an specific event, you can use the "off" function:
+```javascript
+Nerms.off('expire');
+Nerms.off('add');
+Nerms.off('destroy');
+```
+
+
+## Dump
+If you want to debug or just want to use all the data as an Object from an entity. You may use the "Dump" function so you can achieve this:
+```javascript
+//Find the entity
+var entity = Nerms.models.User.find.id(1);
+
+//Dump entity
+var object = Nerms.dump(entity);
+
+console.log(object); //Will ouput: { id: 1, firstname: 'Kris', lastname: 'Kuiper' }
+```
+
+###### \**Important note*\*
+If you pass multiple Entities to the "dump" function, it will return an Array with objects
---
+++ index.js
@@ -0,0 +1,484 @@
+'use strict';
+
+var Nerms = new function() {
+
+	var _data = {
+
+		instance : this,
+
+		expire : {
+
+			entities : [],
+			timer 	 : null,
+			interval : 1000
+		},
+
+		events : {
+
+			types : {
+
+				expire : [],
+				destroy : [],
+				create : []
+			},
+
+			fire : function(type, entity) {
+
+				for(var i in _data.events.types[type]) {
+					_data.events.types[type][i](entity);
+				}
+			}
+		},
+
+		type : {
+ 
+            event : function(type) {
+ 
+                if(undefined === _data.events.types[type]) {
+                    throw 'Type "' + type + '" is not a known event type';
+                }
+            },
+ 
+            entity : function(type) {
+ 
+                if(undefined === _data.instance.entities[type]) {
+                    throw 'Entity "' + type + '" does not exists';
+                }
+            }
+        },
+
+		relations : {}
+	};
+
+	//Entities
+	_data.instance.entities = {};
+
+	//Models
+	_data.instance.models = {};
+
+	//Type
+	_data.instance.type = {
+
+		add : function(type) {
+
+			//Create model
+			_data.instance.models[type.name] = new function() {
+
+				var _entities = [];
+				var _instance = this;
+
+				_instance.push = function(entity) {
+					
+					if(entity instanceof _data.instance.entities[type.name]) {
+						return _entities.push(entity);
+					}
+
+					throw 'Entity needs to be an instance of "' + type.name + '"';
+				}
+
+				_instance.list = function() {
+					return _entities;
+				}
+
+				_instance.find = {};
+
+				for(var i in type.properties) {
+
+					(function(property) {
+						
+						_instance.find[property] = function(value, strict) { 
+
+							var found = [];
+
+							for(var a in _entities) {
+
+								if((strict && _entities[a][property].get() === value) || (!strict && _entities[a][property].get() == value)) {
+									found.push(_entities[a]);
+								}
+							}
+
+							if(1 === found.length) {
+								return found[0];
+							}
+
+							if(0 < found.length) {
+								return found;
+							}
+
+							return null;
+						};
+					})(i);
+				}
+
+				return _instance;
+			};
+			
+			//Create entity
+			_data.instance.entities[type.name] = function Entity(data) {
+
+				var _instance = this;
+
+				//Properties
+				var _properties = JSON.parse(JSON.stringify(type.properties));
+
+				//Functions
+				for(var i in type.properties) {
+					
+					(function(property) {
+
+						_instance[property] = {};
+
+						//Push
+						if(true === Array.isArray(_properties[property])) {
+
+							//List
+							_instance[property].list = function() {
+								return _properties[property];
+							}
+
+							_instance[property].push = function(entity) {
+								
+								_properties[property].push(entity);
+								return _instance;
+							}
+
+							return;
+						}
+
+						//Get
+						_instance[property].get = function() {
+							return _properties[property];
+						}
+
+						//Set
+						_instance[property].set = function(value) {
+
+							_properties[property] = value;
+							return _instance;
+						}
+					})(i);
+				}
+
+				_instance.constructor.prototype.toString = function() { 
+					return type.name; 
+				}
+
+				if(data) {
+
+					for(var a in _instance) {
+
+						if(_instance[a] && _instance[a].set) {
+							_instance[a].set(data[a]);
+						}
+					}
+				}
+
+				_data.instance.models[type.name].push(_instance);
+
+				return _instance;
+			};
+
+			return {
+
+				link : function(relation) {
+					
+					//Model validation
+					if(undefined === relation.model) {
+						throw 'Link: Model property is required';
+					}
+
+					if(undefined === _data.instance.models[relation.model]) {
+						throw 'Link: Model "' + relation.model + '" does not exists';
+					}
+
+					//On property validation
+					if(undefined === relation.on) {
+						throw 'Link: Property "on" is required';
+					}
+
+					if(false === Array.isArray(relation.on) || relation.on.length < 2) {
+						throw 'Link: Property "on" must be an Array with two String elements';
+					}
+
+					if(undefined === _data.instance.models[type.name].find[relation.on[1]]) {
+						throw 'Link: Model "'+ type.name +'" does not contain the property "'+ relation.on[1] +'" in "on" property';
+					}
+
+					//Property property validation
+					if(undefined === relation.property) {
+						throw 'Link: Property "property" is required';
+					}
+
+					if(false === Array.isArray(relation.property) || relation.property.length < 2) {
+						throw 'Link: Property "property" must be an Array with two String elements';
+					}
+
+					if(undefined === _data.instance.models[type.name].find[relation.property[0]]) {
+						throw 'Link: Model "'+ type.name +'" does not contain the property "'+ relation.property[0] +'" in "propery" property';
+					}
+
+					//Create relation if not exists
+					if(undefined === _data.relations[type.name]) {
+						_data.relations[type.name] = {now : [], later : []};
+					}
+
+					//Add relation
+					_data.relations[type.name].now.push(relation);
+				}
+			};
+		},
+
+		destroy : function(type) {
+
+			//Delete entities
+			var entities = _data.instance.models[type].list();
+
+			while(entities.length > 0) {
+				_data.instance.destroy(entities[0]);
+			}
+
+			//Delete model object
+			delete _data.instance.models[type];
+
+			//Delete entity object
+			delete _data.instance.entities[type];
+		}
+	}
+
+	//Mapper
+	_data.instance.map = function(objects, type) {
+ 
+        _data.type.entity(type);
+
+		//If objects are not an array (singel entity)
+		if(false === Array.isArray(objects)) {
+			objects = [objects];
+		}
+
+		for(var i in objects) {
+
+			var entity = new _data.instance.entities[type]();
+
+			//Set entity attributes
+			for(var a in entity) {
+
+				if(entity[a] && entity[a].set) {
+					entity[a].set(objects[i][a]);
+				}
+			}
+
+			//Relations
+			if(_data.relations[type]) {
+
+				for(var a in _data.relations[type].now) {
+
+					var relation = _data.relations[type].now[a];
+					var ent 	 = _data.instance.models[relation.model].find[relation.on[1]](objects[i][relation.on[0]]);
+
+					if(null !== ent) {
+
+						entity[relation.property[0]].push(ent);
+						ent[relation.property[1]].push(entity);
+					}
+					else {
+
+						if(undefined === _data.relations[relation.model]) {
+							_data.relations[relation.model] = {now : [], later : []};
+						}
+
+						var relation = JSON.parse(JSON.stringify(relation));
+
+						//console.log(objects[i], relation.on[1])
+
+						relation.value = [objects[i][relation.on[1]], entity];
+						_data.relations[relation.model].later.push(relation);
+					}
+				}
+
+				var remove 	 = [];
+
+				for(var a in _data.relations[type].later) {
+
+					var relation = _data.relations[type].later[a];
+					var ent 	 = _data.instance.models[type].find[relation.on[1]](relation.value[0]);
+
+					if(null !== ent) {
+
+						if(entity[relation.on[1]].get() === ent[relation.on[1]].get()) {
+
+							entity[relation.property[1]].push(relation.value[1]);
+							relation.value[1][relation.property[0]].push(entity);
+							remove.push(a);
+						}
+					}
+				}
+
+				for(var a in remove) {
+					_data.relations[type].later.splice(remove[a], 1);
+				}
+			}
+
+			//Execute create callbacks
+			_data.events.fire('create', entity);
+		}
+
+		return _data.instance;
+	}
+
+	//Expire
+	_data.instance.expire = function(entity, seconds) {
+
+		var date = new Date();
+			date.setSeconds(date.getSeconds() + seconds);
+
+		//Push new entry
+		_data.expire.entities.push({entity : entity, date : date});
+
+		if(null === _data.expire.timer) {
+
+			_data.expire.timer = setInterval(function() {
+
+				var timestamp = new Date().getTime();
+
+				for(var i in _data.expire.entities) {
+
+					if(_data.expire.entities[i].date.getTime() <= timestamp) {
+						
+						//Execute expire callbacks
+						_data.events.fire('expire', _data.expire.entities[i].entity);
+
+						//Destroy entity
+						_data.instance.destroy(_data.expire.entities[i].entity);
+					}
+				}
+
+			}, _data.expire.interval)
+		}
+
+		return _data.instance;
+	}
+
+	//Destroy
+	_data.instance.destroy = function(entity) {
+
+		if(!Array.isArray(entity)) {
+			entity = [entity];
+		}
+
+		for(var a in entity) {
+
+			for(var b in _data.instance.models) {
+
+				//Delete entity from models
+				var entities = _data.instance.models[b].list();
+				var index 	 = entities.indexOf(entity[a]);
+
+				if(index >= 0) {
+					
+					//Execute destroy callbacks
+					_data.events.fire('destroy', entity[a]);
+					
+					entities.splice(index, 1);
+				}
+
+				//Delete entity from within other entities
+				for(var c in entities) {
+
+					for(var d in entities[c]) {
+
+						//Check if its an array
+						if(entities[c][d].push) {
+							
+							var list  = entities[c][d].list();
+							var index = list.indexOf(entity[a]);
+
+							if(index >= 0) {
+								list.splice(index, 1);
+							}
+						}
+					}
+				}
+			}
+
+			//Delete entity from expire
+			for(var e in _data.expire.entities) {
+
+				if(_data.expire.entities[e].entity === entity[a]) {
+					_data.expire.entities.splice(i, 1);
+				}
+			}
+		}
+
+		//Clear expire timer if there are no entities left
+		if(_data.expire.entities.length === 0) {
+			clearInterval(_data.expire.timer);
+		}
+	}
+
+	//Event on
+	_data.instance.on = function(type, callback) {
+
+		_data.type.event(type);
+		_data.events.types[type].push(callback);
+	}
+
+	//Event off
+	_data.instance.off = function(type) {
+
+		_data.type.event(type);
+		_data.events.types[type] = [];
+	}
+
+	_data.instance.dump = function(entities) {
+
+		var dump = [];
+
+		if(entities) {
+			
+			if(false === Array.isArray(entities)) {
+				entities = [entities];
+			}
+
+			for(var a in entities) {
+
+				var plain = {};
+
+				_data.type.entity(entities[a].toString());
+
+				for(var b in entities[a]) {
+
+					if(entities[a][b] && entities[a][b].get && typeof(entities[a][b].get) === 'function') {
+						plain[b] = entities[a][b].get();
+					}
+				}
+
+				dump.push(plain);
+			}
+		}
+
+		if(1 === dump.length) {
+			return dump[0];
+		}
+
+		return dump;
+	}
+};
+
+
+
+var userModel  		 = Nerms.type.add({name : 'User', properties : {id : null, firstname : null, lastname : null, phonenumbers : []}});
+var phonenumberModel = Nerms.type.add({name : 'Phonenumber', properties : {id : null, number : null, users : [] }});
+
+userModel.link({model : 'Phonenumber', on : ['user_id', 'id'], property : ['phonenumbers', 'users']});
+phonenumberModel.link({model : 'User', on : ['phonenumber_id', 'id'], property : ['users', 'phonenumbers']});
+
+
+//Creating new User entity and adding it automatic to the User model
+Nerms.map({id : 1, firstname : 'Kris', lastname : 'Kuiper', 'phonenumber_id' : 1}, 'User');
+
+//Creating new Phonenumber entity and adding it automatic to the Phonenumber model
+Nerms.map({id : 1, number : '0123456789', user_id : 1}, 'Phonenumber');
+
+
+var entity = Nerms.models.User.find.firstname('Kris');
+console.log(entity);
